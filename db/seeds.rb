39aefae1d6796feef76e86185c8f86e50059748b
John M Buscemi <jmbuscemi@gmail.com>
2015-06-19 15:04:57 -0400

    Add styles. Everything looks super awesome

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Activity.create!(name: "Get Groceries", description: "Go to the Grocery Store", due_date: (Time.now + 1.day), position: 1 )
+Activity.create!(name: "Mow the Grass", due_date: (Time.now + 1.day), position: 1 )
+Activity.create!(name: "Clean Bathroom", description: "Shower and toilet need to be cleaned",
+    due_date: (Time.now + 1.day), position: 1 )
