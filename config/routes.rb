c591a4dd9362cfb0ffea53e3badc969cecb5ef9f
Cruz <nunez.a.cruz@gmail.com>
2015-06-19 13:06:20 -0400

    Generate 1 scaffold and 1 model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'activities#index'
+  resources :activities
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
